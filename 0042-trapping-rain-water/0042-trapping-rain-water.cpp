5e90b440fff1e36d93443ab985c0662e687d7f01
Rajpreet Singh <96204032+rajxsv@users.noreply.github.com>
2024-08-07 01:39:40 +0530

    Time: 7 ms (87.2%), Space: 23.2 MB (27.2%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-        int l = 0;
-        int r = arr.size()-1;
-
-        int leftMax = 0;
-        int rightMax = 0;
-        
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> leftMax(n);
+        vector<int> rightMax(n);
         int sum = 0;
-
-        while(l<=r){
-            if(arr[l]<arr[r]){  // shote de hsaab naal paani bharo 
-                if(arr[l]>leftMax){
-                    leftMax = arr[l];
-                }else{
-                    sum += leftMax - arr[l];
-                }
-                l++;
-            }else{
-                if(arr[r]>rightMax){
-                    rightMax = arr[r];
-                }else{
-                    sum+= rightMax - arr[r];
-                }
-                r--;
-            }
+        
+        leftMax[0]=height[0];
+        rightMax[n-1]=height[n-1];
+        
+        for(int i=1; i<n; i++) {
+            leftMax[i] = max(leftMax[i-1],height[i]);
         }
+        
+        for(int i=n-2; i>=0; i--) {
+            rightMax[i] = max(rightMax[i+1],height[i]);
+        }
+        
+        for(int i=0; i<n; i++) {
+            sum += min(leftMax[i],rightMax[i]) - height[i];
+        }
+        
         return sum;
     }
 };
